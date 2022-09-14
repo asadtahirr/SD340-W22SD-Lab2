6b17294a36f661ff008e210e4108357e63b2392c
asad <asadtahir9999@gmail.com>
2022-09-14 12:35:42 -0500

    Lab 2

---
+++ LinkedList.cs
@@ -0,0 +1,54 @@
+﻿namespace SD340_W22SD_Lab2
+{
+    public class Node
+    {
+        public object Data { get; set; }
+        public Node Next { get; set; }
+        public Node(object data) {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.Write(Data);
+        }
+    }
+
+    public class LinkedList
+    {
+        public int Count { get; private set; } = 0;
+        public Node NodeHead { get; set; }
+        public List<Node> Nodes { get; set; }
+        private Type DataType { get; set; }
+        public LinkedList(){
+            Nodes = new List<Node>();
+        }
+
+        public void Add(object nodeData){
+            Type typeOfNewData = nodeData.GetType();
+            Node newNode = new Node(nodeData);
+            if (NodeHead != null)  {
+                if (typeOfNewData == DataType)
+                {
+                    Node previousNode = NodeHead;
+                    newNode.Next = previousNode;
+                }
+                else{
+                    throw new Exception("LinkedList supports only one data type");
+                }
+            }
+            else {
+                DataType = typeOfNewData;
+            }
+            Nodes = Nodes.Prepend(newNode).ToList();
+            NodeHead = newNode;
+            Count += 1;
+        }
+        public void PrintNodes() {
+            Console.WriteLine("\nPrinting all nodes");
+            foreach (Node node in Nodes)  {
+                node.PrintData();
+                Console.Write(", ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using SD340_W22SD_Lab2;
+using System.Collections.Generic;
+
+LinkedList myStringList = new LinkedList();
+myStringList.Add("World");
+myStringList.Add("Station");
+myStringList.PrintNodes();
+LinkedList myIntegerList = new LinkedList();
+myIntegerList.Add(1);
+myIntegerList.Add(2);
+myIntegerList.Add(3);
+myIntegerList.PrintNodes();
+Console.WriteLine("xyz");
